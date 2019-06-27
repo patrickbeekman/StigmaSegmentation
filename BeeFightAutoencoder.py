89681e597182db9b63da7eafa398d353389ee8e5
patrickbeekman <patrickbeekman1095@gmail.com>
2019-06-27 16:21:14 -0400

    sped up the sub frames creation

--- BeeFightAutoencoder.py
+++ BeeFightAutoencoder.py
@@ -610,14 +610,17 @@ def test_with_frame(full_model):
             im_size = 40
             stride = 10
 
+            sub_images = [frame[i:i + im_size, j:j + im_size, :] for i in range(0, h - im_size, stride) for j in range(0, w - im_size, stride)]
+            locations = [(i,j) for i in range(0, h - im_size, stride) for j in range(0, w - im_size, stride)]
+
             # cycle through the frame finding all (im_size X im_size) images with a stride and locations
-            for i in range(0, h - im_size, stride):
-                for j in range(0, w - im_size, stride):
-                    if sub_images is None:
-                        sub_images = np.array([frame[i:i + im_size, j:j + im_size, :]])
-                    else:
-                        sub_images = np.concatenate((sub_images, [frame[i:i + im_size, j:j + im_size, :]]))
-                    locations.append((i, j))
+            # for i in range(0, h - im_size, stride):
+            #     for j in range(0, w - im_size, stride):
+            #         if sub_images is None:
+            #             sub_images = np.array([frame[i:i + im_size, j:j + im_size, :]])
+            #         else:
+            #             sub_images = np.concatenate((sub_images, [frame[i:i + im_size, j:j + im_size, :]]))
+            #         locations.append((i, j))
             # apply standardization and min-max scaling to each sub_image
             sub_images = ((sub_images - tot_mean) / tot_std)
             sub_images[:, :, :, 0] = ((sub_images[:, :, :, 0] - min_max[0]) / (min_max[1] - min_max[0])).clip(0)
